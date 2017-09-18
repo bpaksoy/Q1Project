94620640a067dbfff0a69740232bae1368f4d93d
bpaksoy <paksoybanu83@gmail.com>
2017-09-18 01:06:09 -0400

    Second Commit

--- script.js
+++ script.js
@@ -26,9 +26,11 @@ searchForm.addEventListener("submit", function(event){
        }
      console.log(collegeName + ":" + collegeId)
       if(collegeName && collegeId){
+
         $.get("https://www.nearbycolleges.info/api/everything/" + collegeId, function(data){
 
-          var searchResult = document.querySelector("h1");
+
+          var searchResult = document.querySelector("h4");
           searchResult.textContent = collegeName;
           var schoolStats = document.querySelector("#schoolName");
           var list = document.createElement("ul");
@@ -39,7 +41,7 @@ searchForm.addEventListener("submit", function(event){
           var fulltimeUndergrad = "Fulltime Undergrad Student Population: "+ data.result.enrollment.fulltimeUndergrad;
           var fulltimeGrad = "Fulltime Grad Student Population: " + data.result.enrollment.fulltimeGrad;
           var location = "Main campus: " + data.result.location.city;
-          var tuition = "Annual Tuition: " + data.result.school.instateTuition;
+          var tuition = "Annual Tuition: $" + data.result.school.instateTuition;
           var ratio = "Student to Faculty Ratio: " + data.result.school.studentFacultyRatio
           var actScore = "ACT Average: " + data.result.test.act25;
           var satScore = "SAT Average: " + data.result.test.sat25;
@@ -52,8 +54,9 @@ searchForm.addEventListener("submit", function(event){
           }
           console.log(list);
           schoolStats.appendChild(list);
-          var photoImg = data.result.school.img;
-
+          var photoImgUrl = data.result.school.img;
+          var schoolImg = document.querySelector("#schoolImg");
+          schoolImg.setAttribute("src", photoImgUrl);
 
         });
       }
@@ -64,7 +67,42 @@ searchForm.addEventListener("submit", function(event){
              //console.log("here is alias:", data.result[0].alias, data.result[0].unitid);
              var collegeName2 = data.result[0].name;
              var collegeId2 = data.result[0].unitid;
-             console.log(collegeName2 + ":" + collegeId2)
+             console.log(collegeName2 + ":" + collegeId2);
+
+             if(collegeName2 && collegeId2){
+               $.get("https://www.nearbycolleges.info/api/everything/" + collegeId2, function(data){
+
+                 var searchResult2 = document.querySelector("h4");
+                 searchResult2.textContent = collegeName2;
+                 var schoolStats2 = document.querySelector("#schoolName");
+                 var list2 = document.createElement("ul");
+
+                 var statArray2 = [];
+                 var acceptance2 = "Acceptance rate: " + data.result.admission.acceptanceRate;
+                 var fee2 =  "Application fee: " + data.result.admission.applicationFee;
+                 var fulltimeUndergrad2 = "Fulltime Undergrad Student Population: "+ data.result.enrollment.fulltimeUndergrad;
+                 var fulltimeGrad2 = "Fulltime Grad Student Population: " + data.result.enrollment.fulltimeGrad;
+                 var location2 = "Main campus: " + data.result.location.city;
+                 var tuition2 = "Annual Tuition: $" + data.result.school.instateTuition;
+                 var ratio2 = "Student to Faculty Ratio: " + data.result.school.studentFacultyRatio
+                 var actScore2 = "ACT Average: " + data.result.test.act25;
+                 var satScore2 = "SAT Average: " + data.result.test.sat25;
+                 statArray2.push(acceptance2, fee2, fulltimeGrad2, location2, tuition2, ratio2, actScore2, satScore2);
+                 //console.log("acceptance", acceptance, "fee", fee, "tuition", tuition, statArray);
+                 for(var l = 0; l < statArray2.length; l++){
+                   var li2 = document.createElement("li");
+                   li2.textContent = statArray2[l];
+                   list2.appendChild(li2);
+                 }
+                 console.log(list2);
+                 schoolStats2.appendChild(list2);
+
+                 var photoImgUrl2 = data.result.school.img;
+                 var schoolImg2 = document.querySelector("#schoolImg");
+                 schoolImg2.setAttribute("src", photoImgUrl2);
+
+               });
+             }
            });
        }
 
